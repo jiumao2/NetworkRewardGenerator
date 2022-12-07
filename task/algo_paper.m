a340c542905441254d7f5654230c946fd40826bc
Yue Huang <806628409@qq.com>
2022-12-07 21:02:21 +0800

    Update Network.m

---
+++ task/algo_paper.m
@@ -0,0 +1,58 @@
+classdef algo_paper < handle
+    %ALGO_PAPER Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        prob
+        r_inner
+        observation
+        action
+    end
+    
+    methods
+        function obj = algo_paper()
+            %ALGO_PAPER Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.prob = ones(4,660);
+            obj.r_inner = 5;
+        end
+        
+        function action = compute_action(obj, observation)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            quadrant = getQuadrant(observation);
+            if norm(observation) < obj.r_inner
+                action = 0;
+            else
+                action = obj.random_choose(obj.prob(quadrant,:));
+            end
+            
+            obj.observation = observation;
+            obj.action = action;
+        end
+        
+        function action = sample(obj)
+            action = randi(660);
+        end
+        
+        function update(obj, reward)
+            if obj.action==0
+                return
+            end
+            
+            quadrant = getQuadrant(obj.observation);
+            if reward<=0 && sum(obj.prob(quadrant,obj.action),'all')>1
+                obj.prob(quadrant,obj.action) = obj.prob(quadrant,obj.action)-1;
+            elseif reward>=1
+                obj.prob(quadrant,obj.action) = obj.prob(quadrant,obj.action)+1;
+            end
+        end
+        
+        function out = random_choose(obj, prob)
+            prob = prob./sum(prob);
+            out = find(cumsum(prob)>rand(),1);
+        end
+            
+    end
+end
+
--- task/main_exp2.m
+++ task/main_exp2.m
@@ -1,23 +1,18 @@
 addpath('..\network\')
 
 net_info = load('NeuralNetwork_1205.mat');
-bh_info = load('BehaviorData.mat');
+bh_info = load('BehaviorData_1205.mat');
 env = Network(net_info, bh_info);
+algo = algo_paper();
 
 %%
 observation = env.reset();
 for k = 1:1000000
-    action = compute_action(observation);
+    action = algo.compute_action(observation);
     [observation, reward, done] = env.step(action);
     env.render();
-
+    algo.update(reward);
     if done
         observation = env.reset();
     end
-end
-
-Q = zeros(4, 660);
-
-function action = compute_action(observation)
-    action = randi(660);
 end
